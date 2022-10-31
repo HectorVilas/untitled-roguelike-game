7d278f83445b7cb4aab6d69321c5a5a2e87044e1
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-10-31 19:35:47 -0300

    add url for tile images

--- tiles.js
+++ tiles.js
@@ -3,19 +3,19 @@ import { Tile, Wall } from "./classes.js";
 export const tiles = {
   " ": new Tile("empty", null, false),
   floor: {
-    "w": new Tile("wood", "pending url"),
-    "s": new Tile("stone", "pending url"),
-    "g": new Tile("gravel", "pending url"),
-    "d": new Tile("dirt", "pending url")
+    "w": new Tile("wood", "./media/images/tiles/floor-wood.png"),
+    "s": new Tile("stone", "./media/images/tiles/floor-stone.png"),
+    "g": new Tile("gravel", "./media/images/tiles/floor-gravel.png"),
+    "d": new Tile("dirt", "./media/images/tiles/floor-dirt.png")
     //url example: ./media/images/tiles/dirt.jpg
   },
   wall: {
-    "w": new Wall("wood", "pending url", true),
-    "c": new Wall("concrete", "pending url", true),
-    "+": new Wall("window", "pending url", true),
+    "w": new Wall("wood", "./media/images/tiles/wall-wood.png", true),
+    "c": new Wall("concrete", "./media/images/tiles/wall-concrete.png", true),
+    "+": new Wall("window", "./media/images/tiles/wall-window.png", true),
   },
   ceiling: {
-    "w": new Tile("wood", "pending url"),
-    "l": new Tile("leaves", "pending url"),
+    "w": new Tile("wood", "./media/images/tiles/ceiling-built.png"),
+    "l": new Tile("leaves", "./media/images/tiles/ceiling-leaves.png"),
   },
 };
