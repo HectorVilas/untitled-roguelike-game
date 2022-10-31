e7987d5a81ea5cf85a18dfe50ad9cc14bc3b10c3
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-10-31 20:28:54 -0300

    refresh UI on load

--- handler.js
+++ handler.js
@@ -1,8 +1,8 @@
 import {ui} from "./ui.js";
 import { gameLogic } from "./gameLogic.js";
-import { maps } from "./maps.js";
 import { Player } from "./classes.js";
 
 const player = new Player("Json", {x:5, y:5})
 
 ui.generateBoard();
+ui.refreshBoard();
