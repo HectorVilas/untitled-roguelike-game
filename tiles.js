7bae10a54f6fbb0e4ff3f1757743f04751dca02d
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-10-31 20:30:06 -0300

    rename, toggle class to wall, add grass

--- tiles.js
+++ tiles.js
@@ -1,21 +1,21 @@
-import { Tile, Wall } from "./classes.js";
+import { Tile, Wall, Ceiling } from "./classes.js";
 
 export const tiles = {
   " ": new Tile("empty", null, false),
-  floor: {
+  "floor-tiles": {
     "w": new Tile("wood", "./media/images/tiles/floor-wood.png"),
     "s": new Tile("stone", "./media/images/tiles/floor-stone.png"),
     "g": new Tile("gravel", "./media/images/tiles/floor-gravel.png"),
+    "G": new Tile("grass", "./media/images/tiles/floor-grass.png"),
     "d": new Tile("dirt", "./media/images/tiles/floor-dirt.png")
-    //url example: ./media/images/tiles/dirt.jpg
   },
-  wall: {
+  "walls": {
     "w": new Wall("wood", "./media/images/tiles/wall-wood.png", true),
     "c": new Wall("concrete", "./media/images/tiles/wall-concrete.png", true),
     "+": new Wall("window", "./media/images/tiles/wall-window.png", true),
   },
-  ceiling: {
-    "w": new Tile("wood", "./media/images/tiles/ceiling-built.png"),
-    "l": new Tile("leaves", "./media/images/tiles/ceiling-leaves.png"),
+  "ceiling": {
+    "w": new Ceiling("wood", "./media/images/tiles/ceiling-built.png", 100),
+    "l": new Ceiling("leaves", "./media/images/tiles/ceiling-leaves.png", 50),
   },
 };
