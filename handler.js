3b7728d0e952e0b90d6eb2016a3b530786849c97
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-11-01 01:42:50 -0300

    add temporal code for walking

--- handler.js
+++ handler.js
@@ -1,8 +1,18 @@
 import {ui} from "./ui.js";
 import { gameLogic } from "./gameLogic.js";
-import { Player } from "./classes.js";
-
-const player = new Player("Json", {x:5, y:5})
 
 ui.generateBoard();
-ui.refreshBoard();
+ui.refreshBoard(gameLogic.player.pos);
+ui.refreshSprites(gameLogic.player, [gameLogic.testDummy]);
+
+//for testing
+window.addEventListener("keydown", (e) => {
+  const k = e.key.toLowerCase();
+    if(k == "arrowup") gameLogic.player.pos.y--
+    else if (k == "arrowdown") gameLogic.player.pos.y++
+    else if(k == "arrowleft") gameLogic.player.pos.x--
+    else if(k == "arrowright") gameLogic.player.pos.x++
+
+    ui.refreshBoard(gameLogic.player.pos);
+    ui.refreshSprites(gameLogic.player, [gameLogic.testDummy]);
+});
