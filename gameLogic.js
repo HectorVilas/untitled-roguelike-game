1ed38f7e62d0757b1eb11b440a717d27f3db212d
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-11-01 01:41:59 -0300

    add player url and player dummy for testing

--- gameLogic.js
+++ gameLogic.js
@@ -1,5 +1,8 @@
-export const gameLogic = (() => {
+import { Player } from "./classes.js";
 
+export const gameLogic = (() => {
+  const player = new Player("Json", {x:0, y:0}, "./media/images/sprites/arroba.png");
+  const testDummy = new Player("test", {x:1, y:1}, "./media/images/sprites/arroba.png");
   
-  return {  };
+  return { player, testDummy };
 })();
