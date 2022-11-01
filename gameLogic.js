a00e9a913f99ea9405900217b7c4f1cf5f880a01
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-11-01 18:00:33 -0300

    function to get tile info

--- gameLogic.js
+++ gameLogic.js
@@ -1,8 +1,19 @@
 import { Player } from "./classes.js";
+//test
+import { maps } from "./maps.js";
+import { tiles } from "./tiles.js";
 
 export const gameLogic = (() => {
   const player = new Player("Json", {x:0, y:0}, "./media/images/sprites/arroba.png");
   const testDummy = new Player("test", {x:1, y:1}, "./media/images/sprites/arroba.png");
   
-  return { player, testDummy };
+  function getTile(layer, x, y){
+    const layers = ["floor-tiles", "walls", "ceiling"];
+    const char = maps.testMap?.[layer]?.[y]?.[x];
+    const values = tiles[layers[layer]][char];
+    
+    return values
+  };
+
+  return { player, testDummy, getTile };
 })();
