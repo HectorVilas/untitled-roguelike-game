a5b0def84f1ace6a08703858f729f7b32489ac3d
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-11-07 19:45:52 -0300

    allow pixel scale and board size change

--- uI.js
+++ uI.js
@@ -1,8 +1,16 @@
 import { gameLogic } from "./gameLogic.js";
 
 export const ui = (() => {
-  let boardSize = 15;
-  const dom = {
+  const scale = {
+    boardSize: 21,
+    tilesetWidth: 24,
+    tilePixelRes: 12,
+    pixelMultiplier: 3,
+    getDomTileSize: () => scale.tilePixelRes * scale.pixelMultiplier,
+    getDomBoardSize: () => scale.getDomTileSize() * scale.boardSize,
+  };
+
+  const domLayers = {
     layer0: document.querySelector("#layer0"), //floor-tiles
     layer1: document.querySelector("#layer1"), //floor-sprites
     layer2: document.querySelector("#layer2"), //walls and sprites
@@ -10,13 +18,14 @@ export const ui = (() => {
   }
 
   function generateBoard(){
-    for(let value in dom) {
-      const layer = dom[value];
+    for(let value in domLayers) {
+      const layer = domLayers[value];
 
-      layer.style.gridTemplateColumns = `repeat(${boardSize}, 1fr)`;
-      layer.style.gridTemplateRows = `repeat(${boardSize}, 1fr)`;
+      layer.replaceChildren();
+      layer.style.gridTemplateColumns = `repeat(${scale.boardSize}, 1fr)`;
+      layer.style.gridTemplateRows = `repeat(${scale.boardSize}, 1fr)`;
       
-      for(let i = 0; i < boardSize * boardSize; i++){
+      for(let i = 0; i < scale.boardSize * scale.boardSize; i++){
         const tile = document.createElement("div");
         tile.classList.add("tile");
 
@@ -30,11 +39,11 @@ export const ui = (() => {
     for(let layer = 0; layer < 3; layer++){
       const tilesInDom = document.querySelectorAll(`#layer${layer} .tile`);
 
-      for(let y = 0; y < boardSize; y++){
-        for(let x = 0; x < boardSize; x++){
-          const coordToIdx = y * boardSize + x;
-          const offsetX = x + (playerPos.x - Math.floor(boardSize/2));
-          const offsetY = y + (playerPos.y - Math.floor(boardSize/2));
+      for(let y = 0; y < scale.boardSize; y++){
+        for(let x = 0; x < scale.boardSize; x++){
+          const coordToIdx = y * scale.boardSize + x;
+          const offsetX = x + (playerPos.x - Math.floor(scale.boardSize/2));
+          const offsetY = y + (playerPos.y - Math.floor(scale.boardSize/2));
           const tile = gameLogic.getTile(layer, offsetX, offsetY);
 
           if(tile !== undefined){
@@ -51,16 +60,16 @@ export const ui = (() => {
   function refreshSprites(player, spriteList){
     const layer = document.querySelectorAll("#layer1 .tile");
 
-    const x = Math.floor(boardSize/2);
-    const y = Math.floor(boardSize/2) * boardSize;
+    const x = Math.floor(scale.boardSize/2);
+    const y = Math.floor(scale.boardSize/2) * scale.boardSize;
     layer[x + y].style.backgroundImage = `url(${player.url})`;
 
     spriteList.forEach(sprite => {
-      const x = sprite.pos.x +  Math.floor(boardSize/2) - player.pos.x;
-      const y = (sprite.pos.y + Math.floor(boardSize/2) - player.pos.y) * boardSize;
+      const x = sprite.pos.x +  Math.floor(scale.boardSize/2) - player.pos.x;
+      const y = (sprite.pos.y + Math.floor(scale.boardSize/2) - player.pos.y) * scale.boardSize;
       
-      const onScreenX = x >= 0 && x < boardSize;
-      const onScreenY = y/boardSize >= 0 && y/boardSize < boardSize;
+      const onScreenX = x >= 0 && x < scale.boardSize;
+      const onScreenY = y/scale.boardSize >= 0 && y/scale.boardSize < scale.boardSize;
 
       if(onScreenX && onScreenY) {
         layer[x + y].style.backgroundImage = `url(${sprite.url})`;
@@ -68,5 +77,26 @@ export const ui = (() => {
     });
   };
 
-  return { generateBoard, refreshBoard, refreshSprites };
+  function domResizePixels(pixelResize){
+    if(pixelResize === -1 && scale.pixelMultiplier === 1) return;
+    const tiles = document.querySelectorAll(".tile");
+    const board = document.querySelector("#board")
+
+    scale.pixelMultiplier += parseInt(pixelResize);
+    board.style.width = `${scale.getDomBoardSize()}px`;
+    board.style.height = `${scale.getDomBoardSize()}px`;
+    tiles.forEach(tile => {
+      tile.style.width = `${scale.getDomTileSize()}px`; 
+      tile.style.height = `${scale.getDomTileSize()}px`; 
+    })
+  };
+
+  function domBoardTiles(resize){
+    scale.boardSize += resize;
+    generateBoard();
+    domResizePixels(0);
+  };
+
+  return { generateBoard, refreshBoard, refreshSprites, domResizePixels,
+    domBoardTiles };
 })();
