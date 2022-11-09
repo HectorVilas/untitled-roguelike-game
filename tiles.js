90556692f4c0b94b7c380c96d8df8b7402ee0d3e
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-11-09 15:44:02 -0300

    add more tiles

--- tiles.js
+++ tiles.js
@@ -9,11 +9,18 @@ export const tiles = {
     "g4": new Floor("overgrown grass", "./gfx/tileset.png", { c:13, r:4 }),
     "d": new Floor("dirt", "./gfx/tileset.png", { c:13, r:5 }),
     "p": new Floor("pebbles", "./gfx/tileset.png", { c:13, r:6 }),
+    "r1": new Floor("rock", "./gfx/tileset.png", { c:17, r:1 }),
+    "r2": new Floor("rock", "./gfx/tileset.png", { c:17, r:2 }),
+    "t1": new Floor("tile", "./gfx/tileset.png", { c:17, r:3 }),
+    "t2": new Floor("tile", "./gfx/tileset.png", { c:17, r:4 }),
+    "t3": new Floor("tile", "./gfx/tileset.png", { c:17, r:5 }),
+    "t4": new Floor("tile", "./gfx/tileset.png", { c:17, r:6 }),
     //built
     "w1": new Floor("wood", "./gfx/tileset.png", { c:13, r:7 }),
     "w2": new Floor("wood", "./gfx/tileset.png", { c:13, r:8 }),
     "w3": new Floor("wood", "./gfx/tileset.png", { c:13, r:9 }),
     "w4": new Floor("wood", "./gfx/tileset.png", { c:13, r:10 }),
+    "mc": new Floor("catwalk", "./gfx/tileset.png", { c:13, r:11 }),
     //street
     "s": new Floor("street", "./gfx/tileset.png", { c:15, r:1 }),
     "sp": new Floor("street pothole", "./gfx/tileset.png", { c:15, r:2 }),
