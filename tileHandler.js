c4c84d208d2bb1b689d4197bc188b51ffc5eecee
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-11-14 15:50:57 -0300

    refactor wall connections

--- tileHandler.js
+++ tileHandler.js
@@ -14,25 +14,22 @@ export function getTile(layer, x, y, map){
     if(thisMap?.[layer]?.[y]?.[x+1] !== " ") connected += "e";
     if(thisMap?.[layer]?.[y+1]?.[x] !== " ") connected += "s";
     if(thisMap?.[layer]?.[y]?.[x-1] !== " ") connected += "w";
-    if(thisMap?.[layer]?.[y-1]?.[x+1] !== " " &&
-      thisMap?.[layer]?.[y+1]?.[x+1] !== " " &&
-      thisMap?.[layer]?.[y+1]?.[x-1] !== " " &&
-      thisMap?.[layer]?.[y-1]?.[x-1] !== " " &&
-      connected === "nesw") connected += "+";
     
-    if(connected === "n"
-    || connected === "s"
-    || connected === "ns") { values.colRow.c += 1;
-    } else if(connected === "ne" ) { values.colRow.r += 3;
-    } else if(connected === "es" ) { values.colRow.r += 2;
-    } else if(connected === "sw" ) { values.colRow.r += 2; values.colRow.c += 1;
-    } else if(connected === "nw" ) { values.colRow.r += 3; values.colRow.c += 1;
-    } else if(connected === "new" ) { values.colRow.r += 5;
-    } else if(connected === "nes" ) { values.colRow.r += 4;
-    } else if(connected === "esw" ) { values.colRow.c += 1; values.colRow.r += 4;
-    } else if(connected === "nsw" ) { values.colRow.c += 1; values.colRow.r += 5;
-    } else if(connected === "nesw" ) { values.colRow.r += 6;
-    } else if(connected === "nesw+" ) { values.colRow.r += 6; values.colRow.c += 1; } 
+    if(connected === "n") { values.colRow.r += 3; values.colRow.c += 0;
+    } else if(connected === "e") { values.colRow.r += 3; values.colRow.c += 1;
+    } else if(connected === "s") { values.colRow.r += 1; values.colRow.c += 0;
+    } else if(connected === "w") { values.colRow.r += 3; values.colRow.c += 3;
+    } else if(connected === "ns") { values.colRow.r += 2; values.colRow.c += 0;
+    } else if(connected === "ne" ) { values.colRow.r += 2; values.colRow.c += 1;
+    } else if(connected === "es" ) { values.colRow.r += 0; values.colRow.c += 1;
+    } else if(connected === "ew" ) { values.colRow.r += 2; values.colRow.c += 2;
+    } else if(connected === "sw" ) { values.colRow.r += 0; values.colRow.c += 3;
+    } else if(connected === "nw" ) { values.colRow.r += 2; values.colRow.c += 3;
+    } else if(connected === "new" ) { values.colRow.r += 2; values.colRow.c += 2;
+    } else if(connected === "nes" ) { values.colRow.r += 1; values.colRow.c += 1;
+    } else if(connected === "esw" ) { values.colRow.r += 0; values.colRow.c += 2;
+    } else if(connected === "nsw" ) { values.colRow.r += 1; values.colRow.c += 3;
+    } else if(connected === "nesw" ) { values.colRow.r += 1; values.colRow.c += 2;}
   };
   
   return values
